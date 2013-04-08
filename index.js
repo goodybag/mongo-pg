0d6dc664c1a2a99edea7b876318ebf64a74134e7
John Fawcett <jrf0110@gmail.com>
2013-04-08 18:42:01 -0500

    fixing connection, re-factoring to work as standalone

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var
-  MongoSql  = require('mongo-sql')
-, db        = require('./lib/connection')
+  db        = require('./lib/connection')
+, MongoPg   = require('./lib/mongo-pg')
 ;
 
 module.exports.init = function(config){
   db.init(config);
 
   for (var i = 0, l = config.collections.length; i < l; ++i){
-    module.exports[key] = new MongoPg(config.collections[i]);
+    module.exports[config.collections[i]] = new MongoPg(config.collections[i]);
   }
 };
