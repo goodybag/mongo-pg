0a785204912f6e439130cc9f28a540a699f9e3ec
John Fawcett <jrf0110@gmail.com>
2013-04-28 23:10:23 -0500

    made more sensible interface, no more manual connection pooling

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var
-  db        = require('./lib/connection')
-, MongoPg   = require('./lib/mongo-pg')
-;
-
-module.exports.init = function(config){
-  db.init(config);
-
-  for (var i = 0, l = config.collections.length; i < l; ++i){
-    module.exports[config.collections[i]] = new MongoPg(config.collections[i]);
-  }
-};
+module.exports = require('mongo-pg');
